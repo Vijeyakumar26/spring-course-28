7f3dba9b05064f9e107ab36c6438c1cd495de1ed
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-05-27 21:35:30 +0530

    Eclipse update

--- springboot-MVC/src/main/java/com/springboot/SpringbootMvcApplication.java
+++ springboot-MVC/src/main/java/com/springboot/SpringbootMvcApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootMvcApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMvcApplication.class, args);
 	}
-
+	
 }
